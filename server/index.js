3a4cd88cb856468f798700c73ca8c440d37fc556
Raj-nandan <nandanr532@gmail.com>
2024-09-03 23:38:51 +0530

    resolved some issue

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const PORT = 8000
 const express = require('express')
 const { MongoClient } = require('mongodb')
-const { v4 : uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
-const uri = 'mongodb+srv://nandanr532:nandanr532@cluster0.nex2fmy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+require('dotenv').config();
+const uri = process.env.MONGODB_URI;
 
 
 const app = express()
@@ -14,62 +15,65 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json('hello world')
-})
-
-app.post('/signup', async(req, res) => {
+});
+app.post('/signup', async (req, res) => {
     const client = new MongoClient(uri)
     const { email, password } = req.body
 
     const generatedUserId = uuidv4()
-    const hashedpassword = await bcrypt.hash(password, 10)
+    const hashedPassword = await bcrypt.hash(password, 10)
 
-    try{
-        client.connect()
+    try {
+        await client.connect();
         const database = client.db('tinder-data')
-        const users = database.collection('users') 
+        const users = database.collection('users')
 
         const existingUser = await users.findOne({ email })
 
-        if(existingUser){
-            return res.status(409).send('User already exist')
+        if (existingUser) {
+            return res.status(409).send('User already exists')
         }
 
-        const sanitizerEmail = email.toLowerCase()
+        const sanitizedEmail = email.toLowerCase()
 
         const data = {
-            user_id : generatedUserId,
-            email : sanitizerEmail,
-            hashed_password : hashedpassword
+            user_id: generatedUserId,
+            email: sanitizedEmail,
+            hashed_password: hashedPassword,
         }
-        const insertedUser = await users.insertOne(data)
+
+        await users.insertOne(data)
 
         const token = jwt.sign(insertedUser, sanitizerEmail, {
             expiresIn: 60*24,
         })
 
-        res.status(201).json({ token, userId: generatedUserId, email: sanitizerEmail})
+        res.status(201).json({ token, userId: generatedUserId, email: sanitizedEmail })
 
-    }catch(err){
+    } catch (err) {
         console.log(err)
+        res.status(500).send('Internal Server Error')
+    } finally {
+        await client.close()
     }
-
 })
 
-
-app.get('/users', async(req, res) => {
+app.get('/users', async (req, res) => {
     const client = new MongoClient(uri)
 
-    try{
+    try {
         await client.connect()
         const database = client.db('tinder-data')
         const users = database.collection('users')
 
         const returnedUsers = await users.find().toArray()
         res.send(returnedUsers)
-    }finally{
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('Internal Server Error')
+    } finally {
         await client.close()
     }
-})
-
+});
 
 app.listen(PORT, () => console.log("server is running on port " + PORT))
